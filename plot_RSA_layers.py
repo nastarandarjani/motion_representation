0c24a271d89c6301490d2c04f0575baa17e05522
nastarandarjani <68055679+nastarandarjani@users.noreply.github.com>
2023-10-16 05:31:12 +0330

    Update plot_RSA_layers.py

--- plot_RSA_layers.py
+++ plot_RSA_layers.py
@@ -3,29 +3,34 @@ import pickle
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 from tqdm import tqdm
+from scipy import stats
+from statsmodels.stats.multitest import fdrcorrection
 
-model_name = 'slowfast_r50' # 'x3d_m', 'slowfast_r50'
+model_name = 'x3d_m' # 'x3d_m', 'slowfast_r50'
 
-# Define correlation types, regions of interest, and condition
+# Define correlation types, regions of interest, condition and status
 cor_types = ['pearson', 'spearman', 'euclidean']
 ROIList = ['V1', 'pFS', 'LO', 'EBA', 'MTSTS', 'infIPS', 'SMG']
 condition = ['slow', 'fast', 'fusion', 'rmslow', '']
+stat = ['dynamic', 'static']
 
 # Initialize an empty array for storing max layers
 max_layer = np.empty((len(cor_types), len(ROIList), 2), dtype=object)
 
+color = ['blue', 'red']
 for cond in condition:
     if model_name != 'slowfast_r50' and cond != '':
         continue
     for c, cor in enumerate(cor_types):
         # Create subplots for plotting
-        fig, axes = plt.subplots(2, len(ROIList), figsize=(56, 10), sharey=True)
+        fig, axes = plt.subplots(2, len(ROIList), figsize=(56, 5), sharey = 'row', sharex=True, gridspec_kw={'height_ratios': [50, 1]})
 
         # Loop through regions of interest
         for r, region in enumerate(ROIList):
+            ttest_data = []
+            ax = axes[0, r]
             # Loop through dynamic and static status
-            for s, status in enumerate(['dynamic', 'static']):
-                ax = axes[s, r]
+            for s, status in enumerate(stat):
                 data = []
 
                 # Loop through subjects
@@ -40,7 +45,7 @@ for cond in condition:
                         RSA = pickle.load(File)
 
                     data.append(list(RSA.values()))
-                
+
                 data = np.array(data)
                 filtered_list = list(RSA.keys())
 
@@ -59,23 +64,41 @@ for cond in condition:
                     indices = [index for index, key in enumerate(RSA.keys()) if key in filtered_list]
                     data = data[:, indices]
 
-                SEM = np.std(data, axis=0) / np.sqrt(len(data))
+                # save for ttest
+                ttest_data.append(data)
+                SEM = stats.sem(data, axis=0)
                 # average across subjects
                 data = np.mean(data, axis=0)
 
                 upper_bound = data + SEM
                 lower_bound = data - SEM
 
-                ax.plot(data, color='blue')
-                ax.fill_between(np.array(range(len(data))), lower_bound, upper_bound, color='blue', alpha=0.5)
+                ax.plot(data, color = color[s], label = status)
+                ax.fill_between(np.array(range(len(data))), lower_bound, upper_bound, color=color[s], alpha=0.5, label=None)
 
-                max_key = filtered_list[np.nanargmax(data)]
-                max_layer[c, r, s] = max_key
+                max_layer[c, r, s] = filtered_list[np.nanargmax(data)]
 
-                ax.set_title(f'{status} {region}\n{max_key}')
+            ax.set_ylabel('Correlation (Kendal Tau)')
+            ax.set_title(f'{region}', fontweight='bold')
+            ax.legend()
 
-        fig.supylabel('Correlation (Kendal Tau)')
-        fig.supxlabel('Layer')
+            # perform ttest
+            _, p_values = stats.ttest_ind(ttest_data[0], ttest_data[1], axis=0)
+            p_values = fdrcorrection(p_values, alpha=0.05)[1]
+
+            sig_x = np.where(p_values < 0.05)[0]
+
+            # plot ttest
+            axes[1, r].scatter(sig_x, np.zeros((len(sig_x))), c='black', marker='.', facecolors='none')
+            axes[1, r].set_xticks([])
+            axes[1, r].set_yticks([])
+            axes[1, r].spines['top'].set_visible(False)
+            axes[1, r].spines['right'].set_visible(False)
+            axes[1, r].spines['bottom'].set_visible(False)
+            axes[1, r].spines['left'].set_visible(False)
+            # axes[1, r].set_ylabel('p-value < 0.05', rotation='horizontal', ha='right')
+            axes[1, r].set_xlabel('label')
+
         plt.tight_layout()
         plt.savefig(f'/plot/{model_name}_{cor}_{cond}.png')
         plt.close()
